c5f2abd0103c48fa8cacd933485791d1f65eb2f1
Fernando Costa <ferndcosta353@gmail.com>
2023-06-22 21:54:43 -0300

    feat: add hero and monster data and getDiceRollArray function

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
+const hero = {
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceCount: 3
+}
+
+const monster = {
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceCount: 1
+}
+
+function getDiceRollArray(diceCount) {
+    return new Array(diceCount).fill(0).map(function () {
+        return Math.floor(Math.random() * 6) + 1
+    });
+}
+
