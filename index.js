cec0ab42a0eb10f8817b638df318254ea2dea7c4
Fernando Costa <ferndcosta353@gmail.com>
2023-06-26 21:58:35 -0300

    perf: moved getDiceHtml function inside of Character function as a method function

--- index.js
+++ index.js
@@ -20,31 +20,29 @@ function getDiceRollArray(diceCount) {
     });
 }
 
-function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount).map(function (num) {
-        return `<div class="dice">${num}</div>`
-    }).join('')
-}
-
 function Character(data) {
     Object.assign(this, data)
-
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;
-        const diceHtml = getDiceHtml(diceCount)
-
-        document.getElementById(elementId).innerHTML =
-            `<div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}" />
-            <div class="health">health: <b> ${health} </b></div>
-            <div class="dice-container">    
-                ${diceHtml}
-            </div>
-        </div>`;
-
+    
+     this.getDiceHtml = function(diceCount) {
+        return getDiceRollArray(diceCount).map(function(num){ 
+            return  `<div class="dice">${num}</div>`
+        }).join('')
     }
 
+    this.getCharacterHtml = function () {
+        const { elementId, name, avatar, health, diceCount } = this;      
+        let diceHtml = this.getDiceHtml(diceCount);
+        
+           return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div>`;
+    }  
 }
 
 const wizard = new Character(hero)
