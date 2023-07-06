5ab4aa0b73531e3c5946dfe13bc7a3605cc1f13f
Fernando Costa <ferndcosta353@gmail.com>
2023-07-06 16:16:10 -0300

    feat: add 'getHealthBarHtml' method function
    return health bar html with the right percentage and width depending on character's health

--- js/Character.js
+++ js/Character.js
@@ -1,9 +1,11 @@
-import {getDiceRollArray, getDicePlaceholderHtml} from "./utils.js"
+import {getDiceRollArray, getDicePlaceholderHtml, getPercentage} from "./utils.js"
 
 function Character(data) {
     Object.assign(this, data)
     
     this.diceArray = getDicePlaceholderHtml(this.diceCount)
+
+    this.maxHealth = this.health
 
     this.getDiceHtml = function() {
         this.currentDiceScore = getDiceRollArray(this.diceCount)
@@ -23,14 +25,27 @@ function Character(data) {
         }
     }
 
+    this.getHealthBarHtml = function () {
+        const percent = getPercentage(this.health, this.maxHealth)
+        
+        return `
+        <div class="health-bar-outer">
+            <div class="health-bar-inner ${percent < 26 ? "danger" : ""} " 
+                        style="width: ${percent}%;">
+            </div>
+        </div>`
+    }
+
     this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount, diceArray } = this;      
+        const { elementId, name, avatar, health, diceCount, diceArray } = this;
+        const healthBar = this.getHealthBarHtml();      
         
            return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
                 <div class="dice-container">
                     ${diceArray}
                 </div>
