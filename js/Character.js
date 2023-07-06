571772ec0fa871829fc4dcbb66c98d16527fd4c9
Fernando Costa <ferndcosta353@gmail.com>
2023-07-06 14:47:05 -0300

    fix: fix currentDiceScore map parameter

--- js/Character.js
+++ js/Character.js
@@ -8,7 +8,7 @@ function Character(data) {
     this.getDiceHtml = function() {
         this.currentDiceScore = getDiceRollArray(this.diceCount)
         
-        this.diceArray = this.currentDiceScore.map((map) => 
+        this.diceArray = this.currentDiceScore.map((num) => 
             `<div class="dice">${num}</div>`).join('')
     }
 
