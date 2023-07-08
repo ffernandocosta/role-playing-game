bd9ef49d5176523e71db84882d6f86a969824026
Fernando Costa <ferndcosta353@gmail.com>
2023-07-07 21:02:29 -0300

    chore: add comments explaining what each line of code does

--- js/Character.js
+++ js/Character.js
@@ -1,4 +1,4 @@
-import {getDiceRollArray, getDicePlaceholderHtml, getPercentage} from "./utils.js"
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from "./utils.js"
 
 class Character {
     constructor(data){
@@ -6,27 +6,41 @@ class Character {
         this.diceHtml = getDicePlaceholderHtml(this.diceCount)
         this.maxHealth = this.health
     }
+    // The constructor function of the Character class
+    // It takes an object 'data' as a parameter
+    // Object.assign() is used to assign the properties of the 'data' object to the instance of the class (this)
+    // The diceHtml property is initialized with the result of calling the getDicePlaceholderHtml function with the diceCount property as an argument
+    // The maxHealth property is set to the initial value of the health property
 
     setDiceHtml() {
         this.currentDiceScore = getDiceRollArray(this.diceCount)
+        // Assigning the result of calling the getDiceRollArray function with the diceCount property as an argument to the currentDiceScore property
         
         this.diceHtml = this.currentDiceScore.map((num) => 
             `<div class="dice">${num}</div>`).join('')
+        // Generating an HTML string by mapping each number in currentDiceScore to a <div> element with the number as its content
+        // The map() method creates an array of HTML strings
+        // The join() method combines the array of HTML strings into a single string and assigns it to the diceHtml property
     }
 
     takeDamage(attackScoreArray) {
         const totalAttackScore = attackScoreArray.reduce((total, currentNum) => total + currentNum)
-
+        // Calculating the total attack score by summing up all the numbers in the attackScoreArray
+        
         this.health -= totalAttackScore
+        // Subtracting the totalAttackScore from the health property
         
         if (this.health <= 0){
             this.dead = true;
             this.health = 0;
         }
+        // Checking if the health is less than or equal to 0
+        // If so, setting the dead property to true and the health property to 0
     }
 
     getHealthBarHtml() {
         const percent = getPercentage(this.health, this.maxHealth)
+        // Calculating the percentage of health based on the current health and the maximum health
         
         return `
         <div class="health-bar-outer">
@@ -34,13 +48,19 @@ class Character {
                         style="width: ${percent}%;">
             </div>
         </div>`
+        // Returning an HTML string representing a health bar
+        // The width of the inner div is set based on the calculated percentage
+        // If the percentage is less than 26, the "danger" class is added to the inner div
     }
 
     getCharacterHtml() {
         const { elementId, name, avatar, health, diceCount, diceHtml } = this;
-        const healthBar = this.getHealthBarHtml();      
+        // Destructuring the properties of the instance into individual variables
         
-           return `
+        const healthBar = this.getHealthBarHtml();
+        // Calling the getHealthBarHtml() method to get the HTML string representing the health bar
+        
+        return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
@@ -50,6 +70,8 @@ class Character {
                     ${diceHtml}
                 </div>
             </div>`;
+        // Returning an HTML string representing the character card
+        // It includes the character's name, avatar, health, health bar, and dice container
     }  
 }
 
--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,34 @@
-function getDiceRollArray(diceCount) {  
+function getDiceRollArray(diceCount) {
+    // A function that generates an array of dice roll values based on the diceCount parameter.
+    // The diceCount parameter specifies the number of dice rolls to generate.
+    
     return new Array(diceCount).fill(0).map(() =>
         Math.floor(Math.random() * 6) + 1
-    );   
+    );
+    // Creating a new array with diceCount elements.
+    // Filling the array with initial values of 0.
+    // Using the map() method to generate a random number between 1 and 6 for each element in the array.
+    // Returning the resulting array of dice roll values.
 }
 
 function getDicePlaceholderHtml(diceCount){
+    // A function that generates an HTML string representing a placeholder for dice based on the diceCount parameter.
+    
     return new Array(diceCount).fill(0).map(() => 
         `<div class="placeholder-dice"></div>`
-    ).join('')
+    ).join('');
+    // Creating a new array with diceCount elements.
+    // Filling the array with initial values of 0.
+    // Using the map() method to generate a string containing a placeholder <div> element for each element in the array.
+    // Joining the array of placeholder strings into a single string using an empty string as the separator.
+    // Returning the resulting HTML string representing the dice placeholders.
 }
 
-const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth
+const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth;
+// A arrow function that calculates the percentage based on the remainingHealth and maximumHealth parameters.
+// It multiplies the remainingHealth by 100 and then divides it by the maximumHealth.
+// The result is the calculated percentage.
 
-export {getDiceRollArray, getDicePlaceholderHtml, getPercentage}
+export {getDiceRollArray, getDicePlaceholderHtml, getPercentage};
+// Exporting the getDiceRollArray, getDicePlaceholderHtml, and getPercentage functions
+// to make them available for use in other modules.
